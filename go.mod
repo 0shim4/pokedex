cf9f854f99b5259fe69b89d4bb732845a8f7d4cf
TakatokiOshima <ki13sd6p@gmail.com>
2022-06-25 09:19:57 +0900

    add: 分割用のコード

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/0shim4/pokedex
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Pokedex struct {
+	Pokemon []Pokemon `json:"pokemons"`
+}
+
+type Pokemon struct {
+	No    string `json:"no"`
+	Name  string `json:"name"`
+	Image string `json:"image"`
+}
+
+func main() {
+	raw, err := ioutil.ReadFile("docs/index.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var fc Pokedex
+
+	json.Unmarshal(raw, &fc)
+
+	os.Mkdir("docs/data", 0777)
+
+	for id, ft := range fc.Pokemon {
+		dir := fmt.Sprintf("./docs/data/%v", id)
+		os.Mkdir(dir, 0777)
+
+		file, _ := json.MarshalIndent(ft, "", " ")
+		filePath := dir + "/index.html"
+		err = ioutil.WriteFile(filePath, file, 0644)
+		if err == nil {
+			fmt.Println(filePath)
+		}
+	}
+}
